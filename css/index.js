06aaa194fb8e9eb77732020d4cb0a3ef70c5295e
lemibk <lemibidikasenbeta@gmail.com>
2024-05-17 20:05:01 +0300

    sidebar navigation

--- css/index.js
+++ css/index.js
@@ -1,5 +1,16 @@
 
-function showElement() {
-    element = document.querySelector('.sidebar');
-    element.style.display = 'flex';
+function openside() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function closeside() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+function elementclose() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+function see() {
+    alert("njfn");
 }
